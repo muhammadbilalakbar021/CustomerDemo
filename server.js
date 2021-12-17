ede1d37b2dac3e50df2713fe5c66727a79da6ae0
Bilal Akbar <muhammadbilalakbar021@gmail.com>
2021-12-17 21:38:43 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/customer-demo"));
+app.use(express.static(__dirname + "/dist/CustomerDemo"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/customer-demo/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/CustomerDemo/index.html"));
 });
 app.listen(process.env.PORT || 8080);
