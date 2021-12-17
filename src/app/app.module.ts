2633cc14a0ade77c9c2afc4fc9fb3a457ce0fcff
Bilal Akbar <muhammadbilalakbar021@gmail.com>
2021-12-17 18:35:29 +0500

    customer components, utils, models, helpers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddCustomerComponent } from './components/add-customer/add-customer.component';
+import { ViewCustomerComponent } from './components/view-customer/view-customer.component';
+import { DetailCustomerComponent } from './components/detail-customer/detail-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCustomerComponent,
+    ViewCustomerComponent,
+    DetailCustomerComponent
   ],
   imports: [
     BrowserModule,
